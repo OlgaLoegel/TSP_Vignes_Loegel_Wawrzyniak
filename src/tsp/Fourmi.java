1e82034bdd714ee8d77a71f4ba6a39b27797b1da
jmloe <jmloe@172.17.2.198>
2018-10-19 15:21:52 +0200

    Class Fourmi

--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -3,7 +3,7 @@ package tsp;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Fourmi {
+public class Ant {
 	private Instance m_instance;
 	private int originCity;
 	private ArrayList<Integer> visitedCities;        // toutes les villes visitées par la fourmi
@@ -11,16 +11,42 @@ public class Fourmi {
     private int currentPosition;
     private long tmpVisitedLength;
     
-    public Fourmi(Instance m_instance) {
+    public Ant(Instance m_instance) {
     	super();
     	this.m_instance=m_instance;
     	this.visitedCities= new ArrayList<Integer>();
     	for (int i=1; i<=m_instance.getNbCities(); i++) {
     	     this.visitedCities.add(i);}
-    	this.originCity= (int)Math.ceil((Math.random()*m_instance.getNbCities()));
-        this.tmpVisitedLength=0;
-    }
+    	this.originCity= this.originCity;
+        this.tmpVisitedLength=0;}
     
+    
+   public ArrayList<Integer> getVisitedCities(){
+        return this.visitedCities; 
+}
+   public ArrayList<Integer> getCitiesStillVisited(){
+	   return this.citiesStillToVisit;
+   }
+  
+   public long getTmpVisitedLength() {
+ 		return tmpVisitedLength;
+ 	}
    
-    
+   public int getCurrentPosition() {
+	   return this.currentPosition;
+   }
+
+    public void setVisitedCities(ArrayList<Integer> visitedCities) {
+	   this.visitedCities = visitedCities;
+}
+
+  
+	public void setTmpVisitedLength(long tmpVisitedLength) {
+		this.tmpVisitedLength = tmpVisitedLength;
+	}
+
+
+	public void setCurrentPosition(int currentPosition) {
+	   this.currentPosition = currentPosition;
+}
 }
