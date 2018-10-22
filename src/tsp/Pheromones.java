4fd48298c78ed7ed361a2ac824c86ff4aca29583
jmloe <jmloe@DESKTOP-6SGRT4M>
2018-10-22 10:56:20 +0200

    pher

--- src/tsp/Pheromones.java
+++ src/tsp/Pheromones.java
@@ -2,40 +2,19 @@ package tsp;
 
 public class Pheromones  {
 	
-	/*Paramètres*/
-	public Instance m_instance;
-	public int m_nbCities; 
-	public int Q;                 /*Taux d'évaporation*/
-	public double[][] pheromones; /* Tableau indiquant la quantité de pheromones entre chaque ville i et j*/
-	public double evaporation;    /* Taux d'évaporation */
-	public Ant[] Antsystem;       /* Tableau constitué de l'ensemble des fourmis*/
+
+
+public int DeposedPheromones(int i, int j, Ant[] AntSystem, int Q) {         /*Si la fourmis a parcouru i->j ou j->i, retourne la quantité de pheromones déposés sur l'arc i-j, sinon retourne 0.*/
+	int s=0;
+	for (Ant ant : AntSystem) {
+		s+=ant.getWentThisPath(i, j)*Q/ant.getVisitedLength();
+	}return s;
 	
-	public Pheromones(Instance m_instance, int m_nbCities,  int Q, double[][] pheromones, double evaporation,
-	Ant[] Antsystem) {
-		super();
-		this.m_instance=m_instance;
-		this.m_nbCities=m_nbCities; 
-		this.Q=Q;
-		this.pheromones=pheromones;  
-		this.evaporation=evaporation;
-		this.Antsystem=Antsystem;
-		
-	}
-	
-	/*Methodes*/
-	
-	public int DeposedPheromones(int i, int j, Ant[] AntSystem) {         /*Si la fourmis a parcouru i->j ou j->i, retourne la quantité de pheromones déposés sur l'arc i-j, sinon retourne 0.*/
-		int s=0;
-		for (Ant ant : AntSystem) {
-			s+=ant.getWentThisPath(i, j)*Q/ant.getVisitedLength();
-		}return s;
-		
-	}
-	
-	public void setPheromones( Ant[] AntSystem, double[][] pheromones) {       /*Met a jour la quantite de pheronomes present sur tous les arcs*/
-		for (int i=0; i<m_nbCities; i++) {
-	        for (int j=0; j<m_nbCities; j++){
-	            pheromones[i][j] = pheromones[i][j]*(100-evaporation)/100 + this.DeposedPheromones(i, j, AntSystem);
-	            pheromones[j][i] = pheromones[j][i]*(100-evaporation)/100 + this.DeposedPheromones(j, i , AntSystem);  
-	}}}
 }
+
+public void setPheromones( Ant[] AntSystem, double[][] pheromones, double evaporation, int Q) {       /*Met a jour la quantite de pheronomes present sur tous les arcs*/
+	for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++){
+            pheromones[i][j] = pheromones[i][j]*(100-evaporation)/100 + this.DeposedPheromones(i, j, AntSystem, Q);
+            pheromones[j][i] = pheromones[j][i]*(100-evaporation)/100 + this.DeposedPheromones(j, i , AntSystem, Q);  
+}}}
